147a7e7c8ded41f497ad48d48ee7dd95153403e9
JaydenZ <jzoll@umass.edu>
2025-12-01 12:05:01 -0500

    Udated Gyro and Flick it

--- main.ino
+++ main.ino
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050.h>
+
+#define VRX_PIN 15
+#define VRY_PIN 13
+
 
 void setup() { Serial.begin(115200); }
 
@@ -13,7 +19,7 @@ Speaker : 0.40 inch Circle Button: 0.50 inch Circle Infared: 0.22 by 0.4 in Rect
 
 
 void bopIt() { static const byte buttons[] = {34, 0, 35};
-static bool initialized = false; static int prev = 0x00; static unsigned long timeout = 0; int data = 0;
+static bool initialized = false; static int prev = 0x00; static unsigned long timeout = 0; int data = 0; 
 
 if (!initialized) { for (byte i = 0; i < 3; i++) pinMode(buttons[i], INPUT_PULLUP); initialized = true; }
 
@@ -23,15 +29,128 @@ if (data != prev || millis() > timeout) { if (data != 0) { Serial.println("Butto
 
 delay(10);  }
 
-void sliceIt() { static const int irPin = 27;
-static bool initialized = false;
+// const int IR_D0 = 32;
 
-if (!initialized) { pinMode(irPin, INPUT); initialized = true; }
+// void irTest() {
+//   static bool initialized = false;
 
-int irValue = digitalRead(irPin);
+//   if (!initialized) {
+//     Serial.println("IR Sensor Test Started...");
+//     pinMode(IR_D0, INPUT);
+//     analogReadResolution(12);
+//     analogSetAttenuation(ADC_11db);
+//     initialized = true;
+//   }
 
-if (irValue == LOW) { Serial.println("Object detected!"); } else { Serial.println("No object detected."); }
+//   int digitalValue = digitalRead(IR_D0);
 
-delay(200);  }
+//   if (digitalValue == HIGH) {
+//     Serial.println("NO OBJECT");
+//   } else {
+//     Serial.println("OBJECT DETECTED");
+//   }
 
-void loop() { bopIt(); sliceIt(); }
+//   delay(100);
+// }
+
+void gyroTest() {
+    MPU6050 mpu;
+  static bool initialized = false;
+
+  if (!initialized) {
+    Serial.println("Initializing MPU6050...");
+    Wire.begin(21, 22);
+    mpu.initialize();
+
+    if (!mpu.testConnection()) {
+      Serial.println("MPU6050 connection failed!");
+    } else {
+      Serial.println("MPU6050 connected!");
+    }
+
+    initialized = true;
+  }
+
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  if (az > 8000) {
+    Serial.println("Orientation: UPRIGHT");
+  } else if (az < -8000) {
+    Serial.println("Orientation: UPSIDE DOWN");
+  } else {
+    Serial.println("Orientation: SIDEWAYS / MOVING");
+  }
+
+  delay(200);
+}
+
+
+// void micTest() { static const int MIC_PIN = 34;
+//   static bool initialized = false;
+
+//   if (!initialized) {
+//     Serial.println("Mic Test Started...");
+//     analogReadResolution(12);
+//     analogSetAttenuation(ADC_11db);
+//     initialized = true;
+//   }
+
+//   int micValue = analogRead(MIC_PIN);
+
+//   if (micValue > 2200) {
+//     Serial.println("LOUD");
+//   } else {
+//     Serial.println("SOFT");
+//   }
+
+//   delay(50);
+// }
+
+
+void FlickIt() {
+  static bool initialized = false;
+  static int xNeutral = 0;
+  static int yNeutral = 0;
+
+  const int DEADZONE = 400; // tighten deadzone for better accuracy
+
+  if (!initialized) {
+    Serial.println("Joystick Ready!");
+    xNeutral = analogRead(VRX_PIN); // measure neutral position
+    yNeutral = analogRead(VRY_PIN);
+    initialized = true;
+  }
+
+  int xValue = analogRead(VRX_PIN);
+  int yValue = analogRead(VRY_PIN);
+
+  String direction = "CENTER";
+
+
+  if (xValue < xNeutral - DEADZONE || xValue > xNeutral + DEADZONE|| yValue < yNeutral - DEADZONE || yValue > yNeutral + DEADZONE) direction = "Flicked";
+
+  if (direction != "CENTER") {
+    Serial.println(direction);
+  }
+  delay(200);
+}
+
+
+
+
+
+
+
+
+void loop() {
+  bopIt();
+  gyroTest();
+  // micTest();
+  // irTest();
+  //FlickIt();
+}
+
+
+
+
